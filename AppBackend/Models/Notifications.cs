5e354423a99fae3171c9292125d952adb6023d12
Leandro D. Lemos Murta <Leandro D. Lemos Murta>
2017-07-31 17:48:42 -0300

    Adicionar arquivos de projeto.

---
+++ AppBackend/Models/Notifications.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Azure.NotificationHubs;
+
+namespace AppBackend.Models
+{
+    public class Notifications
+    {
+        public static Notifications Instance = new Notifications();
+
+        public NotificationHubClient Hub { get; set; }
+
+        private Notifications()
+        {
+            Hub = NotificationHubClient.CreateClientFromConnectionString("Endpoint=sb://hubacmenamespace.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=BdYZz2ZusZ208Q5GXTQXbBh3DfHBeYAA30VhSWXYVGc=",
+                                                                         "HubAcme");
+        }
+    }
+}
